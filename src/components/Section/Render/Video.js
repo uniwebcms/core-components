f86685d45caef9d6676c3e6c26df1288dc02eea1
tianyu <tianyu@proximify.ca>
2024-11-25 11:00:28 -0500

    Minor fix;

--- src/components/Section/Render/Video.js
+++ src/components/Section/Render/Video.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaCompress, FaExpand } from 'react-icons/fa';
-import { twJoin, Media, Image, website } from '../../_utils';
+import { twJoin, Media, Image, website, stripTags } from '../../_utils';
 
 const youtubeRegex =
     /\b(?:https?:\/\/)?(?:(?:www|m)\.)?youtu(?:\.be\/|be\.com\/(?:watch(?:\?(?:(?:feature=player_embedded|app=desktop)&)?v=|\/)|v\/|oembed\?url=http%3A\/\/www\.youtube\.com\/watch\?v%3D|attribution_link\?a=[0-9A-Za-z\-_]{10,20}&u=(?:%2F|\/)watch%3Fv%3D|e(?:mbed)?\/|shorts\/)|be-nocookie\.com\/embed\/)([0-9A-Za-z\-_]{10,20})/;
