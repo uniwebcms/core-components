d7dd8cfb03d986fb10c4c0928e4e142afc2d67eb
tianyu <tianyu@proximify.ca>
2025-11-27 14:13:21 -0500

    Minor fix;

--- src/components/Section/Render/Video.js
+++ src/components/Section/Render/Video.js
@@ -56,6 +56,8 @@ function getCoverImgUrl(video) {
         coverUrl =
             new uniweb.Profile(`docufolio/profile`, '_template').getAssetInfo(coverImg.identifier)
                 ?.src || '';
+    } else if (typeof coverImg === 'string') {
+        coverUrl = coverImg;
     }
 
     return coverUrl;
